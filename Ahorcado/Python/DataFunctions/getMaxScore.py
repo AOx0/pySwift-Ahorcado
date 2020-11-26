7415b34e893bb232c23953fd4fff00f3d9a8ca59
AOx0 <50227494+AOx0@users.noreply.github.com>
2020-11-26 00:30:18 -0600

    Added support for a special default word list. Difficulties: 	- Easy: Only words from 'easyWords'. Just 1 hidden word (minimum) 	- Default: Words from 'easyWords' and 'deafultWords'. 70% of the word must be hidden (minimum) 	- Hard: Words from 'easyWords', 'deafultWords' and 'hardWords'. 100% of the word must be hidden.

--- Ahorcado/Python/DataFunctions/getMaxScore.py
+++ Ahorcado/Python/DataFunctions/getMaxScore.py
@@ -5,8 +5,6 @@ userPath = sys.argv[1]
 with open(f"{userPath}/Library/Application Support/AOX0/save.json", "r") as file:
     data = json.load(file)
     
-    os.system(f"touch {userPath}/Library/Application\ Support/AOX0/tempLoader.json")
-    
     print(f"{data['maxScore']['inEasy']}:{data['maxScore']['inDefault']}:{data['maxScore']['inHard']}:")
     file.close()
     
--- Ahorcado/Python/GameFunctions/genWord.py
+++ Ahorcado/Python/GameFunctions/genWord.py
@@ -6,14 +6,13 @@ with open(f"{sys.argv[1]}/Library/Application Support/AOX0/save.json", "r") as f
 
     difficulty = sys.argv[2]
 
-    if difficulty == "default":
-        words = data["wordsList"]["hardWords"] + data["wordsList"]["easyWords"]
-        randomWord = words[random(0, len(words)-1)]
-    elif difficulty == "easy":
-        randomWord = data["wordsList"]["easyWords"][random(0, len(data["wordsList"]["easyWords"])-1)]
+    if difficulty == "hard":
+        words = data["wordsList"]["hardWords"] + data["wordsList"]["easyWords"] + data["wordsList"]["defaultWords"]
+    elif difficulty == "default":
+        words = data["wordsList"]["defaultWords"] + data["wordsList"]["easyWords"]
     else:
-        randomWord = data["wordsList"]["hardWords"][random(0, len(data["wordsList"]["hardWords"])-1)]
-
+        words = data["wordsList"]["easyWords"]
+        
+    randomWord = words[random(0, len(words)-1)]
     print(randomWord)
-    
     file.close()
